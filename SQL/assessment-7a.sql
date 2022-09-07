b570e24cb0438ea26e594fd5d4d5db166d522aa4
Jarvis Home Automation <100192687+JarvisHomeAuto@users.noreply.github.com>
2022-09-07 18:05:21 -0400

    added the three sql queries for assessment number 7. completes issue #6

---
+++ SQL/assessment-7a.sql
@@ -0,0 +1,1 @@
+SELECT * FROM [dbo].[Customer] Where FirstName LIKE 'S%' order by FirstName desc
---
+++ SQL/assessment-7b.sql
@@ -0,0 +1,6 @@
+SELECT Customer.FirstName, Customer.LastName, COALESCE(OrderLine.Cost, 0) * COALESCE(OrderLine.Quantity,0) AS Cost FROM [dbo].[Customer] 
+LEFT JOIN [dbo].[Order]
+on [Order].CustomerID = Customer.CustID
+LEFT JOIN [dbo].[OrderLine]
+on OrderLine.OrdID = [Order].OrderID
+where GETDATE() < DATEADD(month, 6, [Order].OrderDate) or OrderLine.Cost IS NULL
